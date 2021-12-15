c3e8b13973d4635a971735f2db9c45683b6969f8
ShiningBlack <amitzxzx@gmail.com>
2021-12-15 13:13:11 +0200

    Add project files.

---
+++ ElectionSystem2.0/Ballot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionSystem2._0
+{
+    internal class Ballot
+    {
+        public int Id { get; private set; }
+        public string Location { get; private set; }
+        public string Discreption { get; set; }
+        public bool IsOpen { get; set; }
+        private readonly Dictionary<Voter, bool> voters;
+
+        public Ballot(int id, string loc, string discreption)
+        {
+            voters = new Dictionary<Voter, bool>();
+            IsOpen = false;
+            Discreption = discreption;
+            Location = loc;
+            Id = id;
+        }
+        public bool AddVoter(Voter voter)
+        {
+            if(voter.Ballot == null)
+            {
+                voter.Ballot = this;
+                voters.Add(voter, false);
+                return true;
+            }
+            return false;
+        }
+        public bool RemoveVoter(Voter v)
+        {
+            if(voters.Remove(v))
+            {
+                v.Ballot = null;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ElectionSystem2.0/Party.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionSystem2._0
+{
+    internal class Party
+    {
+        public string Name { get; set; }
+        public List<Candidate> Candidates { get; private set; }
+        public Party(string name)
+        {
+            Name = name;
+            Candidates = new List<Candidate>();
+        }
+        public bool AddCandidate(Candidate candidate)
+        {
+            if(candidate.Party == null)
+            {
+                Candidates.Add(candidate);
+                return true;
+            }
+            return false;
+        }
+        public bool RemoveCandidate(Candidate candidate)
+        {
+
+            if (Candidates.Remove(candidate))
+            {
+                candidate.Party = null;
+                return true;
+            }
+            return false;
+        }
+    }
+}
